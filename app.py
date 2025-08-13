4338a7e8cd0abae89cb74afa62d2564874d3febe
Aysh2171 <aysh2171@gmail.com>
2025-08-14 00:51:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -577,5 +577,5 @@ def image_proxy():
         print(f"Error proxying image: {e}")
         return 'Failed to fetch image', 500
 '''
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
